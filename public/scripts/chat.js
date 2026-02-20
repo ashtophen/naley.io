8602185621b30d7c334857f6df7cbf02e12ba0a9
ashtophen <nathenperley@gmail.com>
2026-02-19 19:41:36 -0500

    skeleton of chatroom has been setup.

---
+++ public/scripts/chat.js
@@ -0,0 +1,8 @@
+const chatBox = document.getElementById("chatbox");
+const chatInput = document.getElementById("chatinput");
+/* I need to figure out my variable names in localStorage.
+const username = localStorage.getItem("name"); */
+
+function messageSend() {
+
+}
